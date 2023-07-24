b4e5d22dbb95c155ee91d090b0c849563b9cc941
ldoo22 <luis.ricardo.do.o@gmail.com>
2023-07-24 18:07:01 +0100

    Added some options

--- options.lua
+++ options.lua
@@ -1,5 +1,9 @@
 return {
   opt = {
-    showtabline = 0
+    showtabline = 0,
+    gcr = 'n-v-c-sm:block,i-ci-ve:ver25,r-cr-o:hor20,n:blinkwait700-blinkoff400-blinkon250',
+    colorcolumn = '80',
+    swapfile = false,
+    clipboard = ''
   },
 }
